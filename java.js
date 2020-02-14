289a0dfcd93201494d246feabeb71a19b97e822b
VilleGrundstrom <59997735+VilleGrundstrom@users.noreply.github.com>
2020-02-14 10:29:42 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,41 @@
+/*
+var search = document.getElementById("search");
+var nappi = document.getElementById("nappi");
+var li = document.getElementsByTagName("li");
+var ul = document.querySelector("ul");
+*/
+
+/*function search(){
+return search.value.length;
+}
+*/
+/*
+function li(){
+return li.length;
+}
+*/
+/*
+function search(){
+if(search()>0){
+search();}
+}
+
+function x(event){
+if(nappi()>0 &&event.which==13){
+nappi();}
+}*/
+
+/* Pääfunktio alkaa */
+/*
+function syotto() {
+var li = document.createElement("li");
+li.appendChild(document.createTextNode(x.value));
+ul.appendChild(li);
+search.value="";
+}
+*/
+/* Pääfunktio loppuu */
+/*
+search.addEventListener("click", search);
+search.addEventListener("keypress", search);
+*/
