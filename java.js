0993ba6af2e3b4ab7bc8317a0b15642452fa817f
VilleGrundstrom <59997735+VilleGrundstrom@users.noreply.github.com>
2020-03-16 15:15:22 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,41 +1,39 @@
-/*
+
 var search = document.getElementById("search");
 var nappi = document.getElementById("nappi");
 var li = document.getElementsByTagName("li");
 var ul = document.querySelector("ul");
-*/
 
-/*function search(){
+function search(){
 return search.value.length;
 }
-*/
-/*
+
 function li(){
 return li.length;
 }
-*/
-/*
+
+
 function search(){
 if(search()>0){
 search();}
 }
 
-function x(event){
+function search(event){
 if(nappi()>0 &&event.which==13){
 nappi();}
-}*/
+}
 
 /* Pääfunktio alkaa */
-/*
-function syotto() {
+
+function addBtn() {
 var li = document.createElement("li");
-li.appendChild(document.createTextNode(x.value));
+li.appendChild(document.createTextNode(ul.value));
 ul.appendChild(li);
 search.value="";
 }
-*/
+
 /* Pääfunktio loppuu */
-/*
+
 search.addEventListener("click", search);
 search.addEventListener("keypress", search);
-*/
+
